7caec7ecabf3a58f699fe10c7c44a8b88aa24040
Aditya Srivastava <aditya@pipixel.io>
2024-10-11 12:30:13 +0530

    Updated code
    Updated code

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import LoginImage from "../assets/login.png";
+import LogoImage from "../assets/logo.png";
+import TextField from "@mui/material/TextField";
 
 const Login = () => {
   return (
     <>
-      <div className="">
+      <div className="relative w-full h-screen">
+        <img
+          className="w-full h-screen object-cover"
+          src={LoginImage}
+          alt="Login"
+        />
 
-        <img className="w-full h-screen " src={LoginImage} alt="Login" />
-        
-        <div className="flex justify-center items-center">
-          
+        <div className="absolute inset-0 flex justify-center items-center">
+          <div className="bg-white rounded-lg shadow-lg w-[1042px] h-[752px] p-8">
+            <div className="flex justify-center items-center mt-16 flex-wrap flex-col">
+              <img src={LogoImage} alt="LogoImage" />
+
+              <h2 className="text-2xl font-bold mb-4">Welcome</h2>
+              <p className="text-gray-200">Login to setter with AI</p>
+
+              <div className="w-[339px] space-y-6">
+                <TextField
+                  className=""
+                  id="outlined-basic"
+                  label="Outlined"
+                  variant="outlined"
+                  width
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
