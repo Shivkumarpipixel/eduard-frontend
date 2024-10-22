a5ad276fdb55349a845abda11a21898371635df6
Shivkumarpipixel <shiv@pipixel.io>
2024-10-22 17:14:56 +0530

    Update Header.jsx

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState ,useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Dropdown, Button } from 'react-bootstrap';
 
@@ -31,7 +31,12 @@ const Header = ({ siderBarFn }) => {
       </div>
 
       <div className="top_header_inner flex items-center gap-2.5">
-
+        <button className="text-gray-400 hover:text-gray-500">
+          {/* Bell icon SVG */}
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+          </svg>
+        </button>
         <div className="relative" ref={dropdownRef}>
           <button
             onClick={() => setIsOpen(!isOpen)}
